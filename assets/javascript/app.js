d12e7a974545de427e3baad01c01cd2f8d0d51be
Christopher Ruiz <chrisruizaz@gmail.com>
2019-09-02 22:43:00 -0700

    psuedo coded questions and things I need help with

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,6 +21,49 @@ function start() {
     clockRunning = true;
   }
 }
+//How do I get my timer to stop counting down?
+//How do I make my gifs pop up with "right" or "wrong" responses?
+//How do I make the next questions come up in the HTML after the GIF?
+//Do I need to make an Array and For Loop to hold the questions and call it with a function and index
+//to populate a new question?.
+//Do I need to gut most of my code to make a for loop work?
+
+//Quiz Questions
+//
+//
+///////////
+//Who does Arya Stark Marry?
+//a Ramsey Bolton
+//b Petyr Baelish
+//c Gendry Baratheon
+//d None of the Above
+//
+//ANSWER D Sansa marries Ramsey Bolton
+///////////
+//How does the Night King die?
+//a Drogon's fire
+//b John Snow's Long Claw
+//c Arya's Dagger (Catspaw)
+//d Samwell Tarly's Dragonglass
+//
+//ANSWER c Arya's Dagger (Catspaw)
+////////////
+//Which Dragon is not one of Daenerys's Dragon?
+//a Viserion
+//b Dracarys
+//c Drogon
+//d Rhaegal
+//
+//ANSWER b Dracarys is the command to breath fire
+////////////
+//Who kills Gregor Clegane?
+//a Sandor Clegane
+//b Oberyn Martell
+//c Maester Qyburn
+//d Arya Stark
+//
+//ANSWER a Sandor Clegane
+/////////////
 
 function count() {
   time--;
